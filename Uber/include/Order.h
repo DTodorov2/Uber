d56f3a01529f9f775ba68ee731e516fc554d4bd9
DTodorov2 <danailtodorov440@gmail.com>
2024-08-09 13:32:16 +0300

    Updating Order

--- Uber/include/Order.h
+++ Uber/include/Order.h
@@ -6,42 +6,35 @@
 class Order
 {
 	bool accepted, finished;
-	//std::string driverName;
-	//std::string carNum;
-	//std::string driverPhoneNum;
 	Driver* driver; // shte sochi kum driver-a, koito e priel poruchkata, ne iskam da kopiram neshtata postoqnno
-	size_t idOrder;
+	//size_t idOrder;
 	Address startAddress;
 	Address destAddress;
 	size_t passengersNum;
 	size_t minutesTillArrival;
-	std::string nameOrderPerson;
-	//int idOrderPerson; //tova nz dali mi trqq
+	std::string nameOwner;
+	size_t idOwner; // posle shte mi pomogne za po-burz dostup ot strana na sistemata kum klientite.
 	//BIG 6?
 public:
 	Order();
-	Order(const Address& startAdd, const Address& finalAdd, size_t numPass, const std::string& nameOrderPerson);
+	Order(size_t idPerson, const Address& startAdd, const Address& finalAdd, size_t numPass, const std::string& nameOrderPerson);
 	Order(const Order& other);
-	int getOrderId() const;
+
+	size_t getIdPerson() const;
 	bool isAccepted() const;
 	bool isFinished() const;
 	double getDriverRating() const;
 	const Address& getStartAddress() const;
 	const Address& getDestAddress() const;
 	size_t getPassengersNum() const;
-	const std::string& getNameOrderPerson() const;
-	//int getIdOrderPerson() const; //nz dali mi trqq vuobshte
+	const std::string& getNameOwner() const;
 	size_t getMinutesTillArrival() const;
 	Driver* getDriver() const;
 
 	void setAccepted(bool isAccepted);
-	//void setDriverName(const std::string& driverName);
-	//void setCarNum(const std::string& carNum);
-	//void setDriverPhoneNum(const std::string& driverPhoneNum);
 	void setMinutes(int minutes);
-	void setDriver(const Driver* driver);
-	void setIdOrder(int idOrder) const;
+	void setDriver(Driver* driver);
+	//void setIdOrder(int idOrder);
 	void setAddress(const Address& newAddress, Address& AddressToChange);
-	//void setDestAddress(const Address& destAddress);
 	void setPassengersNum(size_t passNum);
 };
--- Uber/src/Order.cpp
+++ Uber/src/Order.cpp
@@ -5,19 +5,96 @@
 Order::Order()
 {
 	accepted = false;
-	//idOrder++; da vidq dali trqq go praq tuka ili ne
 	passengersNum = 0;
 	minutesTillArrival = 0;
 	finished = false;
 }
 
-Order::Order(const Address& startAdd, const Address& finalAdd, size_t numPass, const std::string& nameOrderPerson) : Order()
+Order::Order(size_t idPerson, const Address& startAdd, const Address& finalAdd, size_t numPass, const std::string& nameOrderPerson) : Order()
 {
-	idOrder = idNum++;
-	setDriverName(nameOrderPerson);
+	this->idOwner = idPerson;
 	setAddress(startAdd, this->startAddress);
 	setAddress(finalAdd, this->destAddress);
 	setPassengersNum(numPass);
 }
 
-//pri destruktora da namalqm idOrder s 1
+//int Order::getOrderId() const
+//{
+//	return idOrder;
+//}
+
+bool Order::isAccepted() const
+{
+	return accepted;
+}
+
+bool Order::isFinished() const
+{
+	return finished;
+}
+
+double Order::getDriverRating() const
+{
+	return driver->getRating();
+}
+
+const Address& Order::getStartAddress() const
+{
+	return startAddress;
+}
+
+const Address& Order::getDestAddress() const
+{
+	return destAddress;
+}
+
+size_t Order::getPassengersNum() const
+{
+	return passengersNum;
+}
+
+const std::string& Order::getNameOwner() const
+{
+	return nameOwner;
+}
+
+size_t Order::getMinutesTillArrival() const
+{
+	return minutesTillArrival;
+}
+
+Driver* Order::getDriver() const
+{
+	return driver;
+}
+
+void Order::setAccepted(bool isAccepted)
+{
+	accepted = isAccepted;
+}
+
+void Order::setMinutes(int minutes)
+{
+	minutesTillArrival = minutes;
+}
+
+void Order::setDriver(Driver* driver)
+{
+	this->driver = driver;
+}
+
+//void Order::setIdOrder(int idOrder)
+//{
+//	this->idOrder = idOrder;
+//}
+void Order::setAddress(const Address& newAddress, Address& AddressToChange)
+{
+	AddressToChange = newAddress;
+}
+
+void Order::setPassengersNum(size_t passNum)
+{
+	this->passengersNum = passNum;
+}
+
+//pri destruktora da namalqm idOrder s 1, ako go polzvam vuobshte
