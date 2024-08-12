7b0d9e8290cb396c4d5261d822c44ce617891d0e
DTodorov2 <danailtodorov440@gmail.com>
2024-08-12 13:07:19 +0300

    Implementing isOnlyDigits and validateNum

--- Uber/include/Helper.h
+++ Uber/include/Helper.h
@@ -9,4 +9,6 @@ class Helper
 {
 public:
 	static void addressInput(const std::string& str1, const std::string& str2, Address& address, char ch = 'd');
+	static bool isOnlyDigits(const std::string& str);
+	static size_t validateNum(const std::string& str, char constraintL, char constraintR);
 };
--- Uber/src/Helper.cpp
+++ Uber/src/Helper.cpp
@@ -21,3 +21,47 @@ void Helper::addressInput(const std::string& str1, const std::string& str2, Addr
 	address.setName(addressName);
 	address.setPoint(coordinateX, coordinateY);
 }
+
+bool Helper::isOnlyDigits(const std::string& str)
+{
+	if (str[0] == '-')
+	{
+		std::cout << "The number must be positive!" << std::endl;
+		return false;
+	}
+	size_t strLen = str.length();
+	if (strLen == 0)
+	{
+		std::cout << "You must enter a value!" << std::endl;
+		return false;
+	}
+	for (size_t i = 0; i < strLen; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			std::cout << "Must contain only digits!" << std::endl;
+			return false;
+		}
+		else if (str[i] == '0' && strLen == 1)
+		{
+			std::cout << "The number must be grater than 0!" << std::endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+size_t Helper::validateNum(const std::string& str, char constraintL, char constraintR)
+{
+	std::string passengers;
+	do
+	{
+		std::cout << str;
+		std::getline(std::cin, passengers);
+		if (passengers.size() >= 2)
+		{
+			continue;
+		}
+	} while (passengers[0] < constraintL || passengers[0] > constraintR);
+	return passengers[0] - '0';
+}
