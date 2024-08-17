c162f6757fba32e1c56305f075d77582544ff33c
DTodorov2 <danailtodorov440@gmail.com>
2024-08-17 19:58:59 +0300

    Upgrading Helper

--- Uber/include/Helper.h
+++ Uber/include/Helper.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include "Address.h"
 
 const int MAX_LENGTH_STRING = 1024;
 static int idNum = 0;
 
+const std::vector<std::string> strings = {
+	"-------------------\n1 - Make an order\n2 - Check your order\n3 - Cancel your order\n4 - Pay\n5 - Add money to your balance\n6 - View profile\n7 - Show my messages\n8 - Logout\n-------------------",
+	"-------------------\n1 - Change your address\n2 - Check your orders\n3 - Accept order\n4 - Decline order\n5 - Finish order\n6 - View profile\n7 - Show my messages\n8 - Logout\n-------------------",
+	"-------------------\nYou must register or login first\n1 - Register\n2 - Login\n3 - Exit\n-------------------"
+};
+
 class Helper
 {
+	static void validateCoordinate(const std::string& coord, double& coordinateNum, std::string& coordinateString);
 public:
 	static void addressInput(const std::string& str1, const std::string& str2, Address& address, char ch = 'd');
 	static bool isOnlyDigits(const std::string& str);
--- Uber/src/Helper.cpp
+++ Uber/src/Helper.cpp
@@ -1,18 +1,38 @@
 #include "../include/Helper.h"
+#include <regex>
+
+void Helper::validateCoordinate(const std::string& coord, double& coordinateNum, std::string& coordinateString)
+{
+	std::regex coordinatePattern("^[+-]?[0-9]+$");
+	std::cout << coord;
+	std::getline(std::cin, coordinateString);
+	while (!std::regex_match(coordinateString, coordinatePattern))
+	{
+		std::cout << "coordX: ";
+		std::getline(std::cin, coordinateString);
+	}
+	coordinateNum = std::stod(coordinateString);
+}
 
 void Helper::addressInput(const std::string& str1, const std::string& str2, Address& address, char ch) // trqbva li da q razdelqm na oshte -> ne mislq
 {
-	std::string addressName, additionalInfo;
-	int coordinateX = 0, coordinateY = 0;
+	std::string addressName, additionalInfo, coordinateXStr, coordinateYStr;
+	double coordinateX = 0, coordinateY = 0;
+	
 	std::cout << str1;
 	std::getline(std::cin, addressName);
-	std::cout << str2;
-	std::cin >> coordinateX;
-	std::cin.ignore();
-	std::cout << "coordY: ";
-	std::cin >> coordinateY;
-	std::cin.ignore();
-	if (ch == 'd')
+	while (addressName.size() == 0)
+	{
+		std::cout << "The destination name must be at least 1 character!" << std::endl;
+		std::cout << str1;
+		std::getline(std::cin, addressName);
+	}
+
+	std::cout << str2 << std::endl;
+	validateCoordinate("coordX: ", coordinateX, coordinateXStr);
+	validateCoordinate("coordY: ", coordinateY, coordinateYStr);
+
+	if (ch != 'd')
 	{
 		std::cout << "Additional info (optional): " << std::endl;
 		std::getline(std::cin, additionalInfo);
@@ -42,11 +62,6 @@ bool Helper::isOnlyDigits(const std::string& str)
 			std::cout << "Must contain only digits!" << std::endl;
 			return false;
 		}
-		else if (str[i] == '0' && strLen == 1)
-		{
-			std::cout << "The number must be grater than 0!" << std::endl;
-			return false;
-		}
 	}
 	return true;
 }
@@ -54,7 +69,7 @@ bool Helper::isOnlyDigits(const std::string& str)
 size_t Helper::validateNum(const std::string& str, char constraintL, char constraintR)
 {
 	std::string passengers;
-	do
+	while (true)
 	{
 		std::cout << str;
 		std::getline(std::cin, passengers);
@@ -62,6 +77,11 @@ size_t Helper::validateNum(const std::string& str, char constraintL, char constr
 		{
 			continue;
 		}
-	} while (passengers[0] < constraintL || passengers[0] > constraintR);
+		if (passengers[0] < constraintL || passengers[0] > constraintR)
+		{
+			continue;
+		}
+		break;
+	}
 	return passengers[0] - '0';
 }
